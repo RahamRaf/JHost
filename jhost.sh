0f35c28d244956f388b44f4cd0dea588c5caa45c
RahamRaf <20357439+RahamRaf@users.noreply.github.com>
2019-11-28 15:16:40 +0100

    Add script variables section

--- jhost.sh
+++ jhost.sh
@@ -2,11 +2,16 @@
 
 # Main script for the JHost application
 
+clear
+
 # Show fancy title for terminal users
 echo "-----------------------------------------------------------------"
 echo "-----------------------      J-Host      ------------------------"
 echo "-----------------------------------------------------------------"
 echo
 
+# Set env vars
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+
 # Run Initiation script
-source ./inc/init.sh
+source ${SCRIPTPATH}/inc/init.sh
