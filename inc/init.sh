aa093f956be7046738c36a092b619f6afc30dce4
RahamRaf <20357439+RahamRaf@users.noreply.github.com>
2019-11-28 15:17:11 +0100

    add language and timezone to initiation

--- inc/init.sh
+++ inc/init.sh
@@ -3,15 +3,24 @@
 # Initiation script
 
 echo
-echo "--- Initiation"
+echo "INFO: Initiation ..."
 echo
 
 # Update packages
 sudo apt-get update
 
+# Upgrade the system packages
+sudo apt-get -y upgrade
+
 # Installing initial packages
-sudo apt-get install bash-completion nano curl wget git python3 
+sudo apt-get install bash-completion nano curl wget git python3
+
+# Initialize timezone
+sudo timedatectl set-timezone Europe/Amsterdam
+
+# Initialize system language
+sudo locale-gen "en_US.UTF-8"
 
 echo
-echo "- Initiation Finished"
+echo "INFO: Initiation Finished."
 echo
