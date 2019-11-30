f589aef0c87e099c81bef006ec0cc4409e940d69
RahamRaf <20357439+RahamRaf@users.noreply.github.com>
2019-11-30 14:24:30 +0100

    Add logger to the install_webserver script

--- bin/install_webserver.sh
+++ bin/install_webserver.sh
@@ -7,17 +7,21 @@
 if [[ "$1" -eq "nginx" ]] || [[ "$1" -eq "n" ]]; then
   # Install system based on Nginx webserver
   # Nginx
+  jlogger "INFO: Installing Nginx"
   sudo apt-get install nginx
 
   # Certbot for SSL Certificates
+  jlogger "INFO: Installing Certbot"
   sudo apt-get install certbot python-certbot-nginx
 
 elif [[ "$1" -eq "apache" ]] || [[ "$1" -eq "a" ]]; then
   # Install system based on Apache webserver
   # Apache
+  jlogger "INFO: Installing Apache2"
   sudo apt-get install apache2 libapache2-mod-php
 
   # Certbot for SSL Certificates
+  jlogger "INFO: Installing Certbot"
   sudo apt-get install certbot python-certbot-apache
 
 else
@@ -28,7 +32,15 @@ else
 fi
 
 # MariaDB
+jlogger "INFO: Installing MariaDB"
 sudo apt-get install mariadb-server mariadb-client mariadb-common
 
 # PHP
+jlogger "INFO: Installing PHP"
 sudo apt-get install php-common php-fpm php-gd php-mysql php-memcached php-memcache php-mbstring php-mail php-imagick php-uuid
+
+# Webserver installation is finished.
+echo
+jlogger "INFO: Webserver installation is finished."
+echo
+exit 1
