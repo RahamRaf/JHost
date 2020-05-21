6c007396f2b13af492f4ec70a611390903f6a117
RahamRaf <20357439+RahamRaf@users.noreply.github.com>
2020-05-21 14:09:20 +0200

    Backup from specific host directory initiative code

--- bin/backup.sh
+++ bin/backup.sh
@@ -1,25 +1,71 @@
 #!/bin/bash
 
-# Backup script for the system or hosts
+# Backup script for the system or (a) host(s)
 
 # Create backup from a web host (input)
 if [ $1 ]; then
 	WEBHOST="$1"
-  # ###############################
-  # Make backup in website folder e.g. /var/www/site.com/backup
-  # Make backup from webroot
+
+	# Server type
+	$SRVT=`cat ${ETCDIR}/server`
+
+	if [[ ! ${SRVT} ]]; then
+		# Webserver is not available
+	  jlogger "FATAL: Can't create backup for host '${WEBHOST}'. Webserver settings '${SRVT}' is not found."
+	  exit 0
+
+	fi
+
+	# Host root folder
+	$HOSTSET=/var/www/${WEBHOST}
+
+	# Check specific host
+	if [ ! -d $HOSTSET ]; then
+		# host is not available
+	  jlogger "FATAL: Can't find the host folder '${HOSTSET}'."
+	  exit 0
+
+	fi
+
+  # Backup folder
+	$BCKFOL=${HOSTSET}/backups
+
+	if [ ! -d $BCKFOL ]; then
+		# Create backup folder
+		sudo mkdir -p $BCKFOL
+	  jlogger "INFO: Backup folder for host '${WEBHOST}' is created."
+
+	fi
+
+	# Set backup file name
+	BCKFILE="${BCKFOL}/backup.${WEBHOST}.`date +%F`.tgz"
+
+	# Backup webroot
+	if [ ! -f ${BCKFILE} ]; then
+
+    jlogger "INFO: Creating host backup '${BCKFILE}'"
+  	sudo tar cpzf ${BCKFILE} --directory=${HOSTSET} --exclude=${BCKFOL}
+
+  else
+		# Backup file existed. No overwrite
+  	jlogger "FATAL: Host backup can't be created. File '${BCKFILE}' is already existed!"
+		exit 0
+
+  fi
+	
+	# ######################################
   # Make backup from database
 
 else # Create system wide backup
 
   # Backup folder
   if [ ! -d $BKDIR ]; then
-          sudo mkdir -p ${BKDIR}
-  	      jlogger "INFO: Backup's folder '${BKDIR}' has been created."
+    sudo mkdir -p ${BKDIR}
+    jlogger "INFO: Backup's folder '${BKDIR}' has been created."
   fi
 
   # Set backup file name
-  BCKFILE="${BKDIR}/backup.`cat /etc/hostname`.`date +%F`.tgz"
+  BCKFILE="${BKDIR}/backup-full.`cat /etc/hostname`.`date +%F`.tgz"
 
   # Create backup if the backup file for today doesn't exists
   if [ ! -f ${BCKFILE} ]; then
@@ -28,7 +74,10 @@ else # Create system wide backup
   	sudo tar cpzf ${BCKFILE} --directory=/ --exclude=/proc --exclude=/tmp --exclude=/lost+found --exclude=${BKDIR} --exclude=/mnt --exclude=/sys --exclude=/var/spool --exclude='*/sock/*' --exclude='*.sock' /
 
   else
+		# Backup file existed. No overwrite
   	jlogger "FATAL: Backup doesn't created. Backup file '${BCKFILE}' is already existed!"
+		exit 0
+
   fi
 
 fi
